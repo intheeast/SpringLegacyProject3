f7c60517d954b1c762f078a912227cb4942aed3c
intheeast <intheeast0725@gmail.com>
2023-12-12 15:50:02 +0900

    add world method

--- src/com/intheeast/dao/UserDao.java
+++ src/com/intheeast/dao/UserDao.java
@@ -2,6 +2,10 @@ package com.intheeast.dao;
 
 public class UserDao {
 	
+	public static void world() {
+		System.out.println("world method");
+	}
+	
 	public static void hello() {
 		System.out.println("hello method");
 	}
